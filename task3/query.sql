f7742864f86c2f637f90c32ed69781d8fbf3ad58
Eli <akanoou@gmail.com>
2024-06-06 20:06:39 +0300

    this is a real sql

--- task3/query.sql
+++ task3/query.sql
@@ -1,33 +1,19 @@
-SELECT
-    u.id AS user_id,
-    u.email,
-    p.name AS property_name,
-    CASE
-        WHEN p.type = 'string' THEN up.value_string
-        WHEN p.type = 'int' THEN CAST(up.value_int AS CHAR)
-        WHEN p.type = 'datetime' THEN CAST(up.value_datetime AS CHAR)
-        END AS property_value
-FROM
-    users u
-        JOIN
-    users_properties up ON u.id = up.user_id
-        JOIN
-    properties p ON up.property_id = p.id
-GROUP BY
-    u.id, u.email, p.name, property_value;
+SET @sql = (
+    SELECT CONCAT(
+               'SELECT u.id AS user_id, u.email, ',
+               GROUP_CONCAT(
+                   DISTINCT CONCAT(
+                   'MAX(CASE WHEN p.name = ''', `name`, ''' THEN ',
+                   'CASE WHEN p.type = ''string'' THEN up.value_string ',
+                   'WHEN p.type = ''int'' THEN up.value_int ',
+                   'WHEN p.type = ''datetime'' THEN DATE_FORMAT(up.value_datetime, ''%Y-%m-%d'') END END) AS `', `name`, '`'
+                            ) ORDER BY `id` SEPARATOR ', '
+               ),
+               ' FROM users u JOIN users_properties up ON u.id = up.user_id JOIN properties p ON up.property_id = p.id GROUP BY u.id, u.email'
+           )
+    FROM properties
+);
 
-SELECT
-    u.id AS user_id,
-    u.email,
-    p.*,
-    MAX(CASE WHEN p.name = 'name' THEN up.value_string END) AS name,
-    MAX(CASE WHEN p.name = 'likes' THEN up.value_string END) AS likes,
-    MAX(CASE WHEN p.name = 'dob' THEN up.value_string END) AS dob
-FROM
-    users u
-        JOIN
-    users_properties up ON u.id = up.user_id
-        JOIN
-    properties p ON up.property_id = p.id
-GROUP BY
-    u.id;
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
